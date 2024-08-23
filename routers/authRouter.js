f1d645289c42d48de47717def89e1fa81c286635
Mohit_kumar_pandey <93579686+22Mohit22@users.noreply.github.com>
2024-08-23 19:08:51 +0530

    updated auth router to handle sign in and sign out

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
-const { getHome, signinPage, signupPage, signupUser, signinUser } = require("../controllers/auth");
+const { getHome, signinPage, signupPage, signupUser, validateSignUp, validateSignIn } = require("../controllers/auth");
+const passport = require("passport");
+const { validationResult } = require('express-validator');
 
 const router = Router();
 
@@ -7,7 +9,39 @@ router.get('/', getHome);
 router.get('/signin', signinPage);
 router.get('/signup', signupPage);
 
-router.post('/signup', signupUser);
-router.post('/signin', signinUser);
+router.post('/signup', validateSignUp, signupUser);
+router.post('/signin', validateSignIn, (req, res, next) => {
+    const errors = validationResult(req);
+    if ( !errors.isEmpty() ) {
+        console.log(errors.array());
+        
+        res.render('signin', {user: null, formData: req.body, errors: errors.array()});
+        return;
+    }
+
+    passport.authenticate("local", (err, user, info) => {
+        if (err) return next(err);
+        if (!user) {
+            const authError = { msg: info.message, path: 'username'};
+            return res.render('signin', { user: null, formData: req.body, errors: [authError] });
+        }
+        req.logIn(user, err => {
+            if (err) {
+                return next(err);
+            }
+            return res.redirect('home');
+        });
+    })(req, res, next);
+});
+
+router.post('/logout', (req, res, next) => {
+    req.logout( (err) => {
+        if (err) {
+            console.log(err);
+            return next(err);
+        }
+        res.redirect('/');
+    })
+})
 
 module.exports = router;
