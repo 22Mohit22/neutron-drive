041b78635df530fdd963b7c2ad13dd2b848e74fd
Mohit_kumar_pandey <93579686+22Mohit22@users.noreply.github.com>
2024-08-29 18:12:12 +0530

    Update homeRouter.js

--- routers/homeRouter.js
+++ routers/homeRouter.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { createFolder, folderForm, afterSignin, getFolderContent, getUpdateFolderForm ,updateFolder, deleteFolder } = require("../controllers/home");
+const { createFolder, folderForm, afterSignin, getFolderContent, getUpdateFolderForm ,updateFolder, deleteFolder, folderNameValidation, editFolderNameValidation } = require("../controllers/home");
 
 const router = Router();
 
@@ -7,10 +7,10 @@ router.get('/', afterSignin)
 router.get('/home', getFolderContent)
 router.get('/folders/:id', getFolderContent)
 router.get('/folders/:id/create', folderForm)
-router.post('/folders/:id/create', createFolder)
+router.post('/folders/:id/create', folderNameValidation, createFolder)
 
 router.get('/folders/:id/update', getUpdateFolderForm)
-router.post('/folders/:id/update', updateFolder)
+router.post('/folders/:id/update', editFolderNameValidation, updateFolder)
 router.post('/folders/:id/delete', deleteFolder)
 
 module.exports = router;
